0346599dabc346f93289200c9032987bd6a1a8a5
manny <manny.acquah@gmail.com>
2014-11-10 22:25:13 +0000

    Fixed toon page and minor styling adjustment

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,27 +3,10 @@
 $(document).foundation();
 
 $(function() {
-    //Enable swiping...
-    $(".display-comic").swipe( {
-        //Generic swipe handler for all directions
-        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-            var img = '';
-            if (direction == 'left') {
-                img = '<img class="" src="http://placehold.it/400x300&text=[left]"/>';
-            } else if (direction == 'right') {
-                img = '<img class="" src="http://placehold.it/400x300&text=[right]"/>';
-            }
-            $(this).html(img);
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:0
-    });
-
-    $('.comics').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
-        dots: false
+    // Resize video to full screen
+    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+    // If you want to keep full screen on window resize
+    $(window).resize(function(){
+        $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
     });
 });
