baf724e5d46b708b599283fcc4de1af2d25dbbad
Manny <manny.acquah@twentysixdigital.com>
2014-11-11 13:10:43 +0000

    Social sharing plugin added in

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -20,13 +20,21 @@ module.exports = [
         "clobbers": [
             "Twitter"
         ]
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js",
+        "id": "nl.x-services.plugins.socialsharing.SocialSharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.dialogs": "0.2.10",
-    "com.phonegap.plugins.twitter": "0.3.2"
+    "com.phonegap.plugins.twitter": "0.3.2",
+    "nl.x-services.plugins.socialsharing": "4.3.10"
 }
 // BOTTOM OF METADATA
 });
